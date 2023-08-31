747b8f9423cd78a65fba35f8c44417f327e5d8cc
Tootin0730 <yisan1004@gmail.com>
2023-08-31 09:59:03 -0400

    Time Complexity - TapeEquilibrium

--- solution.java
+++ solution.java
@@ -1,17 +1,21 @@
 class Solution {
     public int solution(int[] A) {
-        int sum = 0;
-        for (int i = 0; i < A.length; i++){
-            sum = sum + A[i];
+        int rightSum = 0;
+        int leftSum = 0;
+        int result = Integer.MAX_VALUE;
+
+        for(int i=0; i<A.length; i++){
+            rightSum += A[i];
         }
 
-        int total = 0;
-        for (int j = 1; j <= A.length+1; j++){
-            total = total + j;
-        }
-        
-        int result = total - sum;
+        int temp = 0;
 
+        for(int i=1; i<A.length; i++){
+            rightSum -= A[i-1];
+            leftSum += A[i-1];
+            temp = Math.abs(rightSum -leftSum);
+            if(temp<result) result = temp;
+        }
         return result;
     }
 }
