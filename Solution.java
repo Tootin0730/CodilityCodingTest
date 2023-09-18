2c55b96d032742932acddf0eb984a25147ecd0dd
Tootin0730 <yisan1004@gmail.com>
2023-09-18 14:53:47 -0400

    Prefix Sums - GenomicRangeQuery

--- Solution.java
+++ Solution.java
@@ -1,11 +1,23 @@
 class Solution {
-    public int solution(int A, int B, int K) {
-        int b = (B/K) + 1;
-        int a = (A/K) + 1;
-
-        if (A%K == 0) {
-            --a;
+    public int[] solution(String S, int[] P, int[] Q) {
+        int [] C = new int[P.length];
+        for (int i=0; i<P.length; i++) {
+            C[i] = factor(S,P[i],Q[i]);
         }
-        return b-a;
+        
+        return C;
     }
+    
+    public int factor(String S, int i, int j) {  
+		if (S.substring(i,j+1).contains("A")){
+			return 1;
+		}else if (S.substring(i,j+1).contains("C")){
+			return 2;
+		}else if (S.substring(i,j+1).contains("G")){
+			return 3;    
+		}
+
+        return 4;
+    }
+    
 }
