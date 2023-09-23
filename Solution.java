f3a4cb64cacedefdaffffc8858ced90c5ae0c0c5
Tootin0730 <yisan1004@gmail.com>
2023-09-23 11:44:38 -0400

    Sorting - MaxProductOfThree

--- Solution.java
+++ Solution.java
@@ -1,13 +1,5 @@
-import java.util.Arrays;
+function solution(A) {
+    A.sort((a, b) => (a - b));
 
-class Solution {
-	public static int solution(int[] A) {
-		Arrays.sort(A);
-		int dupl=0;
-		for (int i = 1; i < A.length; i++) {
-			if (A[i] == A[i-1])
-				dupl++;
-		}
-		return A.length - dupl;
-	}
+    return Math.max(A[A.length - 1] * A[A.length - 2] * A[A.length - 3], A[A.length - 1] * A[0] * A[1]) ;
 }
