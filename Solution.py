46593732c2fa8715e339a229471bc22e9d01b143
Tootin0730 <yisan1004@gmail.com>
2023-09-25 10:32:46 -0400

    Sorting - Triangle(fix1)
    Fixed version of 'Triangle' by Python

--- Solution.py
+++ Solution.py
@@ -1,11 +1,9 @@
 def solution(A):
-    triangle_num = 0
-    for a in range(len(A)):
-        for b in range(a+1, len(A)):
-            for c in range(b+1, len(A)):
-                if A[a] < A[b] + A[c]:
-                    if A[b] < A[a] + A[c]:
-                        if A[c] < A[a] + A[b]:
-                            triangle_num += 1
-        
-    return triangle_num
+    A_len = len(A)
+    if A_len < 3:
+        return 0
+    A.sort()
+    for index in range(0, A_len-2):
+        if A[index]+A[index+1] > A[index+2]:
+            return 1
+    return 0
