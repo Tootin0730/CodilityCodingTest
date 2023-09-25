fe8f62c98ca1cf124edd8c2d156f9aea1113bd6b
Tootin0730 <yisan1004@gmail.com>
2023-09-25 10:44:48 -0400

    Sorting - Triangle(fix2)
    Fixed version of 'Triangle' by Java SE8

--- Solution.py
+++
@@ -1,9 +0,0 @@
-def solution(A):
-    A_len = len(A)
-    if A_len < 3:
-        return 0
-    A.sort()
-    for index in range(0, A_len-2):
-        if A[index]+A[index+1] > A[index+2]:
-            return 1
-    return 0
