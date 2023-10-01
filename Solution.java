9028c3eaec66ed54d2c53ad9a98a2fb70e4dc01f
Tootin0730 <yisan1004@gmail.com>
2023-10-01 12:00:28 -0400

    Stacks and Queues - Fish

--- Solution.java
+++ Solution.java
@@ -1,53 +1,20 @@
 import java.util.Stack;
 
 class Solution {
-   public int solution(String s) {
-
-        if (s.length() % 2 != 0) {
-            return 0;
+    public int solution(int[] A, int[] B) {
+        Stack<Integer> liveFish = new Stack<>();
+        for(int i = 0; i < A.length; i++) {
+            if (liveFish.isEmpty())
+                liveFish.push(i);
+            else {
+                while (!liveFish.isEmpty() && B[i]-B[liveFish.peek()] == -1 && A[liveFish.peek()] < A[i])
+                    liveFish.pop();
+                if (liveFish.isEmpty())
+                    liveFish.push(i);
+				else if (B[i] - B[liveFish.peek()] != -1)
+                    liveFish.push(i);
+            }
         }
-
-        Character openingBrace = new Character('{');
-        Character openingBracket = new Character('[');
-        Character openingParen = new Character('(');
-        Stack<Character> openingStack = new Stack<Character>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == openingBrace || c == openingBracket || c == openingParen) {
-                openingStack.push(c);
-            } else  {
-                if (i == s.length()-1 && openingStack.size() != 1) {
-                    return 0;
-                }
-                if (openingStack.isEmpty()) {
-                    return 0;
-                }
-                Character openingCharacter = openingStack.pop();
-                switch (c) {
-                case '}':
-                    if (!openingCharacter.equals(openingBrace)) {
-                        return 0;
-                    }
-                    break;
-                case ']':
-                    if (!openingCharacter.equals(openingBracket)) {
-                        return 0;
-                    }
-                    break;
-                case ')':
-                    if (!openingCharacter.equals(openingParen)) {
-                        return 0;
-                    }
-                    break;
-
-                default:
-                    break;
-                }
-            } 
-        }
-
-        return 1;
-
+        return liveFish.size();
     }
 }
