c2ba231f6f94756d2e817b5f082b78fd05e657dd
Tootin0730 <yisan1004@gmail.com>
2023-10-04 20:41:40 -0400

    Stacks and Queues - StoneWall

--- Solution.py
+++ Solution.py
@@ -1,14 +1,21 @@
-def solution(S):
+def solution(H):
+    big_rectangle_stack = []
+    dict_for_cotain_check = {}
 
-    matches, stack = dict(['()', '[]', '{}']), []
-
-    for i in S:
-        if i in matches.values():
-            if stack and matches[stack[-1]] == i:
-                stack.pop()
+    num_of_blocks = 0
+    for h_elm in H:
+        while(True):
+            if len(big_rectangle_stack) != 0 and h_elm < big_rectangle_stack[-1]:
+                val = big_rectangle_stack.pop()
+                del dict_for_cotain_check[val]
             else:
-                return 0
-        else:
-            stack.append(i)
+                break
 
-    return int(not stack)
+        if h_elm in dict_for_cotain_check:
+            pass
+        else:
+            big_rectangle_stack.append(h_elm)
+            num_of_blocks += 1
+            dict_for_cotain_check[h_elm] = 0
+
+    return num_of_blocks    
