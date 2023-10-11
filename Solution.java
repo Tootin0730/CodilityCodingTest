eb68d11b755df74d29c37b53aa06a3769dcb265d
Tootin0730 <yisan1004@gmail.com>
2023-10-11 10:19:42 -0400

    Maximum Slice Problem - MaxProfit

--- Solution.java
+++ Solution.java
@@ -1,16 +1,28 @@
 class Solution {
     public int solution(int[] A) {
-        int max = 0;
+        if (A == null || A.length == 0) return 0;
 
-        for (int i = 0; i < A.length; i++){
-            for(int j = i+1; j < A.length; j++){
-                if (A[j] > A[i]){
-                    if(max < A[j] - A[i]){
-                        max = A[j] - A[i];
-                    }
+        int k = 0, m = 0, p = 0;
+        int k1 = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (k1 > k) {
+                if (A[i] < A[k1]) {
+                    k1 = i;
+                } else if (A[i] - A[k1] >= p) {
+                    k = k1;
+                    m = i;
+                    p = A[i] - A[k];
+                }
+            } else {
+                if (A[i] >= A[m]) {
+                    m = i;
+                    p = A[m] - A[k];
+                } else if (A[i] < A[k]) {
+                    k1 = i;
                 }
             }
         }
-        return max;
+
+        return p;
     }
 }
