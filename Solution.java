789bae0c8cca057b0364078e0fca1f1616f78475
Tootin0730 <yisan1004@gmail.com>
2023-10-16 16:50:09 -0400

    Prime and composite numbers - MinPerimeterRectangle
    Final fixed version

--- Solution.java
+++ Solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int N) {
-        int min = N;
-        for(int a = 1; a < N+1; a++){
-            int b = N/a;
-            int perimeter = 2 * (a + b);
-            if (min > perimeter){
-                min = perimeter;
+        int min = 1+N;
+        int i=1;
+        while(i*i<=N) {
+            if(N % i == 0) {
+                min = Math.min(min, N/i+i);
             }
+            i++;
         }
-        return min;
+        return 2*min;
     }
 }
