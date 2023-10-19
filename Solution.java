84e8ac0bd329424551206d3f0650b3570aedcb12
Tootin0730 <yisan1004@gmail.com>
2023-10-19 15:02:32 -0400

    Sieve of Eratosthenes - CountNonDivisible
    Final fixed version

--- Solution.java
+++ Solution.java
@@ -1,16 +1,46 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] A) {
-        int[] B = {0, 0, 0, 0, 0};
 
-        for(int i = 0; i < A.length; i++){
-            int num = 0;
-            for(int j = 0; j < A.length; j++){
-                if(A[i] % A[j] != 0){
-                    num += 1;
+        HashMap<Integer, Integer> map1 = new HashMap<>();
+        
+        for(int i=0; i< A.length; i++){
+            if(map1.containsKey(A[i]) == false){
+                map1.put(A[i], 1);
+            }
+            else{
+                 map1.put(A[i], map1.get(A[i])+1 );
+            }
+        }
+        
+        HashMap<Integer, Integer> map2 = new HashMap<>();
+        
+        for( int n : map1.keySet() ){            
+            int numDivisors =0;
+            int sqrtN = (int)Math.sqrt(n);
+
+            for(int i=1; i<=sqrtN; i++ ){
+                if( n % i == 0){
+                    int anotherDivisor = n/i; 
+                    if(map1.containsKey(i) == true ){
+                        numDivisors = numDivisors + map1.get(i);
+                    }
+                    if(anotherDivisor != i){
+                        if(map1.containsKey(anotherDivisor) == true){
+                            numDivisors = numDivisors + map1.get(anotherDivisor);
+                        }
+                    }
                 }
             }
-            B[i] = num;
+            int numNonDivisors = A.length - numDivisors;
+            map2.put(n, numNonDivisors); 
         }
-        return B;
+        int[] results = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            results[i] = map2.get(A[i]);
+        }
+
+        return results;
     }
 }
