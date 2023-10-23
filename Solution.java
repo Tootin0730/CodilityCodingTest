d2b8a296cdc9475e8d29ed60d9731fc4ace15e1c
Tootin0730 <yisan1004@gmail.com>
2023-10-23 14:33:05 -0400

    Sieve of Eratosthenes - CountSemiprimes

--- Solution.java
+++ Solution.java
@@ -1,46 +1,45 @@
-import java.util.*;
-
 class Solution {
-    public int[] solution(int[] A) {
-
-        HashMap<Integer, Integer> map1 = new HashMap<>();
+    public int[] solution(int N, int[] P, int[] Q) {
+        int length = P.length;
+        int[] prime = sieve(N);
+        int[] semiprime = semiprime(prime);
+        int[] result = new int[length];
+        int[] semiprimesAggreation = new int[N+1];
         
-        for(int i=0; i< A.length; i++){
-            if(map1.containsKey(A[i]) == false){
-                map1.put(A[i], 1);
-            }
-            else{
-                 map1.put(A[i], map1.get(A[i])+1 );
-            }
+        for(int i=1;i<N+1;i++) {
+            semiprimesAggreation[i] = semiprime[i];
+            semiprimesAggreation[i] += semiprimesAggreation[i-1];    
         }
         
-        HashMap<Integer, Integer> map2 = new HashMap<>();
-        
-        for( int n : map1.keySet() ){            
-            int numDivisors =0;
-            int sqrtN = (int)Math.sqrt(n);
-
-            for(int i=1; i<=sqrtN; i++ ){
-                if( n % i == 0){
-                    int anotherDivisor = n/i; 
-                    if(map1.containsKey(i) == true ){
-                        numDivisors = numDivisors + map1.get(i);
-                    }
-                    if(anotherDivisor != i){
-                        if(map1.containsKey(anotherDivisor) == true){
-                            numDivisors = numDivisors + map1.get(anotherDivisor);
-                        }
-                    }
-                }
+        for(int i=0;i<length;i++) { 
+            result[i] = semiprimesAggreation[Q[i]] - semiprimesAggreation[P[i]] + semiprime[P[i]];
+        }
+        return result;
+    }
+    
+    public int[] sieve(int N) {
+        int[] prime = new int[N+1];
+        for(int i=2; i<=(double)Math.sqrt(N); i++) {
+            if(prime[i] == 0) {
+                int k = i*i;
+                while(k <= N) {
+                    if(prime[k] == 0){
+                        prime[k] = i;    
+                    }                    
+                    k += i;
+                }               
             }
-            int numNonDivisors = A.length - numDivisors;
-            map2.put(n, numNonDivisors); 
         }
-        int[] results = new int[A.length];
-        for (int i = 0; i < A.length; i++) {
-            results[i] = map2.get(A[i]);
-        }
-
-        return results;
+        return prime;
+    }
+    
+    public int[] semiprime(int[] prime) {
+        int semiprime[] = new int[prime.length];
+        for(int i=0;i<prime.length;i++) {
+            if(prime[i] == 0) continue;
+            int firstFactor = prime[i];
+            if(prime[i/firstFactor] == 0) semiprime[i]=1;   
+        }  
+        return semiprime;
     }
 }
