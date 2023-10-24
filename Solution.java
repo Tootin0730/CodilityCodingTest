55de05db2156c78d334533570245829e1f743a6e
Tootin0730 <yisan1004@gmail.com>
2023-10-24 13:36:33 -0400

    Euclidean algorithm - ChocolatesByNumbers
    Final fixed version

--- Solution.java
+++ Solution.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(int N, int M) {
-        int[] X = new int[N];
-        int i = 0;
-        int count = 0;
-        while(X[i] == 0) {
-            X[i] = 1;
-            i = (i + M) % N;            
-            count++;
-        }
-        return count;
+		int lcm = N / gcd(N, M);
+        return lcm;
     }
+    
+    public int gcd(int a, int b) {
+	    if(a % b == 0) return b;
+	    return gcd(b,a%b);
+	}
 }
