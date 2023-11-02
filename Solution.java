851fa1b0ae1435fba6832234d921a440b7fbe4fd
Tootin0730 <yisan1004@gmail.com>
2023-11-02 14:41:26 -0400

    Caterpillar method - AbsDistinct
    Improved version

--- Solution.java
+++ Solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public int solution(int[] A) {
         int sum = 0;
+        int check = 0;
         int minus = 0;
+
         for(int i = 0; i < A.length; i++){
-            if(A[i] < 0){
-                for(int j = i; j < A.length; j++){
-                    if(A[i] == 0-A[j]){
-                        minus += 1;
-                    }
+            check = 0-A[i];
+            for(int j = i+1; j < A.length; j++){
+                if(A[j] == check){
+                    minus += 1;
                 }
-            }
+            } 
             sum += 1;
         }
-        sum = sum - minus;
+        sum = sum-minus;
         return sum;
     }
 }
