5a7e7535f9da624127fe0565d02576772d237f36
Tootin0730 <yisan1004@gmail.com>
2023-11-14 17:06:23 -0500

    Greedy Algorithms - TieRopes

--- Solution.java
+++ Solution.java
@@ -1,21 +1,15 @@
 class Solution {
-    public int solution(int A[], int B[]) {
+    public int solution(int K, int[] A) {
         int N = A.length;
-        if (N <= 1) {
-            return N;
-        }
-        
-        int cnt = 1;
-        int prev_end = B[0];
-        
-        int curr;
-        for (curr = 1; curr < N; curr++) {
-            if (A[curr] > prev_end) {
-                cnt++;
-                prev_end = B[curr];
+        int result = 0;
+        int length = 0;
+        for (int rope : A) {
+            length += rope;
+            if (length >= K) {
+                result++;
+                length = 0;
             }
         }
-        
-        return cnt;
+        return result;
     }
 }
