4a5651ecbc57a006227dd86bd46ad2fedeb81947
Tootin0730 <yisan1004@gmail.com>
2024-01-08 10:20:09 -0500

    Iterations - BinaryGap
    I am re-practicing codility coding test.
    
    Initial try

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
 # CodilityCodingTest
 
-For a given array A of N integers and a sequence S of N integers from the set {−1, 1}, we define val(A, S) as follows:
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by 
+ones at both ends in the binary representation of N.
 
-val(A, S) = |sum{ A[i]*S[i] for i = 0..N−1 }|
-
-(Assume that the sum of zero elements equals zero.)
-
-For a given array A, we are looking for such a sequence S that minimizes val(A,S).
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001
+and contains two binary gaps: one of length 4 and one of length 3. 
+The number 20 has binary representation 10100 and contains one binary gap of length 1. 
+The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
 
 Write a function:
+  class Solution { public int solution(int N); }
+that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
 
-class Solution { public int solution(int[] A); }
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. 
+Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
 
-that, given an array A of N integers, computes the minimum value of val(A,S) from all possible values of val(A,S) for all possible sequences S of N integers from the set {−1, 1}.
 
-For example, given array:
 
-  A[0] =  1
-  A[1] =  5
-  A[2] =  2
-  A[3] = -2
-your function should return 0, since for S = [−1, 1, −1, 1], val(A, S) = 0, which is the minimum possible value.
+
+# NOTE
+I am re-practicing codility coding test.
--- Solution.java
+++ Solution.java
@@ -1,52 +1,22 @@
-public class Solution {
-    public int solution(int[] a) {
-        if (a.length == 0) {
-            return 0;
-        }
-        int sum = 0;
-        int max = Integer.MIN_VALUE;
-        // O(N)
-        for (int i = 0; i < a.length; i++) {
-            final int value = Math.abs(a[i]);
-            sum += value;
-            if (max < value) {
-                max = value;
-            }
-            a[i] = value;
-        }
-        final int[] counts = new int[max + 1];
-        for (int value: a) {
-            counts[value]++;
-        }
-        // O(sum(abs(a)))
-        final int[] r = new int[sum + 1];
-        for (int i = 1; i < r.length; i++) {
-            r[i] = -1;
-        }
-        // outer is O(max(abs(a)))
-        // inner is O(sum(abs(a))) which is less than O(N * max(abs(a)))
-        // we don't care of 0 values
-        for (int i = 1; i < counts.length; i++) {
-            // we check r[j]. if it's not less than 0, then it means we've reached j value with previous steps, so no need to spend current
-            // if it's less than 0, spend 1 current number if r[j - i] has been reached
-            for (int j = 0; j < r.length; j++) {
-                // negative value means we haven't reached this value, so we have to spend 1 current if we can
-                if (r[j] >= 0) {
-                    r[j] = counts[i];
-                } else if (j - i >= 0 && r[j - i] > 0) {
-                    r[j] = r[j - i] - 1;
-                }
-                // the value in r[j] then means how many of the current values are left when we reached the value j
+class Solution {
+    public int solution(int N) {
+        int remainder = 0;
+        int binary_gap = 0;
+        int max_gap = 0;
+        while (N > 0){
+            remainder = N%2;
+            N = N/2;
+
+            if(remainder == 1){
+                binary_gap = 0;
+            }else if(remainder == 0){
+                    binary_gap += 1;
+                    if (max_gap < binary_gap){
+                        max_gap = binary_gap;
+                    }
+                
             }
         }
-        int result = sum;
-        // don't have to traverse all the arrays, since i - the sum of elements. if it's reachable then (sum - i) - reachable as well.
-        // so if the value is reachable then the diff is abs(i - (sum - i)), which is the same as abs(sum - 2 * i)
-        for (int i = 0; i < r.length / 2 + 1; i++) {
-            if (r[i] >= 0 && result > Math.abs(sum - 2 * i)) {
-                result = Math.abs(sum - 2 * i);
-            }
-        }
-        return result;
+        return max_gap;
     }
 }
