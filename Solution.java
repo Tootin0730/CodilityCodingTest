6641e5dd38a09aea18af9410abc65452c20a3e3e
Tootin0730 <yisan1004@gmail.com>
2024-01-10 09:10:10 -0500

    Arrays - OddOccurencesInArray
    Initial try

--- Solution.java
+++ Solution.java
@@ -1,26 +1,21 @@
 class Solution {
-    public int[] solution(int[] A, int K) {
-        
-        // Corner cases to save resources
-         if(K == 0 || A.length <= 0 || A.length == K){
-             return A;
-         }
-         
-         // Loop to traverse K times
-         for(int i=0; i<K; i++){
-             int last = A[A.length - 1]; // Last digit
-             
-             // Loop to traverse A.Length times in swing order,
-             // so that first element can be set
-             for(int j=A.length-1; j>0; j--){
-              A[j] = A[j-1];   
-             }
-             
-             // Set last element
-             A[0] = last;
-         }
-         
-         // Return result
-         return A;
+    public int solution(int[] A) {
+        int N = A.length;
+        int result = 0;
+        int check = 0;
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(A[i] == A[j]){
+                    check += 1;
+                }
+            }
+            if(check == 1){
+                result = A[i];
+                break;
+            }
+            check = 0;
+        }
+        return result;
     }
 }
