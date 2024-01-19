c95bfecf127d06acacc151cfd50dae7eca4865ea
Tootin0730 <yisan1004@gmail.com>
2024-01-19 15:58:39 -0500

    Prefix Sums - CountDiv
    Initial try

--- Solution.java
+++ Solution.java
@@ -1,42 +1,14 @@
 class Solution {
     public int solution(int[] A) {
-
-        int result = 0;
-        int zeroPosition = 0;
-        int onePosition = 0;
-
-        for(int i = 0; i < A.length; i++){
-            if(A[i] == 0){
-                zeroPosition += 1;
-            } else if (A[i] == 1){
-                onePosition += 1;
+        int count = 0;
+        int zeroCount = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0) {
+                zeroCount++;
+            } else if (A[i] == 1) {
+                count += zeroCount;
             }
         }
-
-        int[] zero = new int[2];
-        int[] one = new int[3];
-        zeroPosition = 0;
-        onePosition = 0;
-        
-        for(int i = 0; i < A.length; i++){
-            if(A[i] == 0){
-                zero[zeroPosition] = i;
-                zeroPosition += 1;
-            } else if (A[i] == 1){
-                one[onePosition] = i;
-                onePosition += 1;
-            }
-        }
-        
-        for(int a = 0; a < zero.length; a++){
-            for (int b = 0; b < one.length; b++){
-                if(zero[a] < one[b]){
-                    result += 1;
-                }
-            }
-        }
-
-        
-        return result;
+        return count >= 0 && count <= 1000_000_000 ? count : -1;
     }
 }
