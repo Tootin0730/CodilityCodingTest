e17798a29b6b9c1f81d8943e65400349db20a1c3
Tootin0730 <yisan1004@gmail.com>
2024-01-20 11:34:46 -0500

    Prefix Sums - CountDiv
    Final version

--- Solution.java
+++ Solution.java
@@ -1,13 +1,6 @@
 class Solution {
     public int solution(int A, int B, int K) {
-
-        int total = 0;
-
-        for(int i = A; i <= B; i++){
-            if(i % K == 0){
-                total += 1;;
-            }
-        }
-        return total;
+        int prevCount = A>0 ? (A-1)/K : -1;
+        return B/K - prevCount;
     }
 }
