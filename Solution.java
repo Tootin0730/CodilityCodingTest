88d2a563c59f3d48c1d803b4b1ccc98680450aff
Tootin0730 <yisan1004@gmail.com>
2024-01-23 15:26:09 -0500

    Prefix Sums - MinAvgTwoSlice
    Final version

--- Solution.java
+++ Solution.java
@@ -1,24 +1,18 @@
 class Solution {
     public int solution(int[] A) {
-
-        int min = 100;
-        int result = 0;
-        int avg = 100;
-    
-        for(int i = 0; i < A.length; i++){
-            int sum = A[i];
-
-            for(int j = i+1; j < A.length-1; j++){
-                sum += A[j];    
-                avg = (sum) / (j-i+1);
-
-                if(avg < min){
-                    min = avg;
-                    result = i;
-                    break;
-                }
+        double minAvg = (double)(A[0] + A[1]) / 2.0;
+        int minAvgIndex = 0;
+        int n = A.length;
+        for (int i = 0; i < n-1; i++) {
+            if ((double)(A[i] + A[i+1]) / 2.0 < minAvg) {
+                minAvg = (double)(A[i] + A[i+1]) / 2.0;
+                minAvgIndex = i;
+            }
+            if (i < n-2 && (double)(A[i] + A[i+1] + A[i+2]) / 3.0 < minAvg) {
+                minAvg = (double)(A[i] + A[i+1] + A[i+2]) / 3.0;
+                minAvgIndex = i;
             }
         }
-        return result;
+        return minAvgIndex;
     }
 }
