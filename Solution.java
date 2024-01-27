6b6326073e972354226b2d423b47c5e40d3fe88c
Tootin0730 <yisan1004@gmail.com>
2024-01-27 14:19:17 -0500

    Sorting - NumberOfDiscIntersections
    Final version

--- Solution.java
+++ Solution.java
@@ -1,12 +1,25 @@
 import java.util.Arrays;
-
 class Solution {
+
     public int solution(int[] A) {
-        Arrays.sort(A);
-        int n = A.length-2;
-        for (int i = 0; i < n; i++)
-            if ((long)A[i] + (long)A[i+1] > (long)A[i+2])
-                return 1;
-        return 0;
+        long[] A1 = new long[A.length];
+        long[] A2 = new long[A.length];
+        int i, j = 0, n = A.length;
+        for (i = 0; i < n; i++) {
+            A1[i] = i - (long)A[i];
+            A2[i] = i + (long)A[i];
+        }
+        Arrays.sort(A1);
+        Arrays.sort(A2);
+        long intersectCount = 0;
+        for (i = 0; i < n; i++)
+            while (j < n && A2[i] >= A1[j]) {
+                intersectCount += j;
+                intersectCount -= i;
+                j++;
+            }
+        if (intersectCount > 10000000)
+            intersectCount = -1;
+        return (int)intersectCount;
     }
 }
