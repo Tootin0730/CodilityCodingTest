7599221f9ba94fefd446a119750a55da3c1daa78
Tootin0730 <yisan1004@gmail.com>
2024-02-01 11:16:15 -0500

    Leader - Dominator
    Final version

--- Solution.java
+++ Solution.java
@@ -1,20 +1,30 @@
 class Solution {
     public int solution(int[] A) {
-        int max = 0;
-        
-        for(int i = 0; i < A.length; i++){
-            int candidate = 0;
+        int n = A.length, size = 0, value = 0;
 
-            for(int j = i+1; j < A.length; j++){
-                if(A[i] == A[j]){
-                    candidate += 1;
-                }
-            }
-            
-            if(candidate > max){
-                max = candidate;
+        for (int i = 0; i < n; i++){
+            if (size == 0) {
+                size++;
+                value = A[i];
+            } else {
+                size += (value == A[i]) ? 1 : -1;
             }
         }
-        return max;
+
+        if (size <= 0){
+            return -1;
+        }
+
+        int leader = -1, counter = 0;
+        for (int i = 0; i < n; i++){
+            if (A[i] == value) {
+                counter++;
+                if (leader == -1){
+                    leader = i;
+                }
+            }
+        }
+        
+        return (counter > n / 2) ? leader : -1;
     }
 }
