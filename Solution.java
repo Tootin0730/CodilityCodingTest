acbea66578bcda23e2d7566c18882a4ebc96f408
Tootin0730 <yisan1004@gmail.com>
2024-02-06 11:07:23 -0500

    Maximum slice problem - MaxDoubleSliceSum
    final version

--- Solution.java
+++ Solution.java
@@ -1,30 +1,15 @@
 class Solution {
     public int solution(int[] A) {
-
-        int max = 0;
-
-        for(int x = 0; x < A.length; x++){
-            for(int y = x; y < A.length; y++){
-                for(int z = y; z < A.length; z++){
-                    
-                    int sumA = 0;
-                    int sumB = 0;
-                    
-                    for(int a = x+1; a < y; a++){
-                        sumA += A[a];
-                    }
-                    for(int b = y+1; b < z; b++){
-                        sumB += A[b];
-                    }
-
-                    int sum = sumA + sumB;
-                    if (max < sum){
-                        max = sum;
-                    }
-                }    
-            }    
+        int n = A.length;
+        int[] maxSumStart = new int[n];
+        int[] maxSumEnd = new int[n];
+        for (int i = 1, j = n-2; i < n-1; i++, j--) {
+            maxSumEnd[i] = Math.max(0, maxSumEnd[i-1] + A[i]);
+            maxSumStart[j] = Math.max(0, maxSumStart[j+1] + A[j]);
         }
-
-        return max;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 1; i < n-1; i++)
+            maxSum = Math.max(maxSum, maxSumEnd[i-1] + maxSumStart[i+1]);
+        return maxSum;
     }
 }
