674f00f8b9225f7eba10c6c6045c23092dec5641
Tootin0730 <yisan1004@gmail.com>
2024-02-13 11:00:16 -0500

    Sieve of Eratosthenes - CountNonDivisible
    Final version

--- README.md
+++ README.md
@@ -1,64 +1,36 @@
 # CodilityCodingTest
 
-A non-empty array A consisting of N integers is given.
+You are given an array A consisting of N integers.
 
-A peak is an array element which is larger than its neighbors. More precisely, it is an index P such that 0 < P < N − 1,  A[P − 1] < A[P] and A[P] > A[P + 1].
+For each number A[i] such that 0 ≤ i < N, we want to count the number of elements of the array that are not the divisors of A[i]. We say that these elements are non-divisors.
 
-For example, the following array A:
+For example, consider integer N = 5 and array A such that:
 
-    A[0] = 1
-    A[1] = 2
-    A[2] = 3
-    A[3] = 4
-    A[4] = 3
-    A[5] = 4
-    A[6] = 1
-    A[7] = 2
-    A[8] = 3
-    A[9] = 4
-    A[10] = 6
-    A[11] = 2
-has exactly three peaks: 3, 5, 10.
+    A[0] = 3
+    A[1] = 1
+    A[2] = 2
+    A[3] = 3
+    A[4] = 6
+For the following elements:
 
-We want to divide this array into blocks containing the same number of elements. More precisely, we want to choose a number K that will yield the following blocks:
-
-A[0], A[1], ..., A[K − 1],
-A[K], A[K + 1], ..., A[2K − 1],
-...
-A[N − K], A[N − K + 1], ..., A[N − 1].
-What's more, every block should contain at least one peak. Notice that extreme elements of the blocks (for example A[K − 1] or A[K]) can also be peaks, but only if they have both neighbors (including one in an adjacent blocks).
-
-The goal is to find the maximum number of blocks into which the array A can be divided.
-
-Array A can be divided into blocks as follows:
-
-one block (1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2). This block contains three peaks.
-two blocks (1, 2, 3, 4, 3, 4) and (1, 2, 3, 4, 6, 2). Every block has a peak.
-three blocks (1, 2, 3, 4), (3, 4, 1, 2), (3, 4, 6, 2). Every block has a peak. Notice in particular that the first block (1, 2, 3, 4) has a peak at A[3], because A[2] < A[3] > A[4], even though A[4] is in the adjacent block.
-However, array A cannot be divided into four blocks, (1, 2, 3), (4, 3, 4), (1, 2, 3) and (4, 6, 2), because the (1, 2, 3) blocks do not contain a peak. Notice in particular that the (4, 3, 4) block contains two peaks: A[3] and A[5].
-
-The maximum number of blocks that array A can be divided into is three.
-
+A[0] = 3, the non-divisors are: 2, 6,
+A[1] = 1, the non-divisors are: 3, 2, 3, 6,
+A[2] = 2, the non-divisors are: 3, 3, 6,
+A[3] = 3, the non-divisors are: 2, 6,
+A[4] = 6, there aren't any non-divisors.
 Write a function:
 
-class Solution { public int solution(int[] A); }
+class Solution { public int[] solution(int[] A); }
 
-that, given a non-empty array A consisting of N integers, returns the maximum number of blocks into which A can be divided.
+that, given an array A consisting of N integers, returns a sequence of integers representing the amount of non-divisors.
 
-If A cannot be divided into some number of blocks, the function should return 0.
+Result array should be returned as an array of integers.
 
 For example, given:
 
-    A[0] = 1
-    A[1] = 2
-    A[2] = 3
-    A[3] = 4
-    A[4] = 3
-    A[5] = 4
-    A[6] = 1
-    A[7] = 2
-    A[8] = 3
-    A[9] = 4
-    A[10] = 6
-    A[11] = 2
-the function should return 3, as explained above.
+    A[0] = 3
+    A[1] = 1
+    A[2] = 2
+    A[3] = 3
+    A[4] = 6
+the function should return [2, 4, 3, 2, 0], as explained above.
--- Solution.java
+++ Solution.java
@@ -1,37 +1,93 @@
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-class Solution {
-    public int solution(int[] A) {
-        int n = A.length, peaksCount = 0;
-        if (n < 3)
-            return 0;
-        int[] peaks = new int[n];
-        for (int i = 1; i < n-1; i++)
-            if (A[i] > A[i-1] && A[i] > A[i+1]) {
-                peaks[peaksCount] = i;
-                peaksCount++;
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int A[] = new int[5];
+        A[0] = 3;
+        A[1] = 1;
+        A[2] = 2;
+        A[3] = 3;
+        A[4] = 6;
+
+        Solution s = new Solution();
+        int B[] = s.solution(A);
+        System.out.println("Input  : "+Arrays.toString(A));
+        System.out.println("Result : "+Arrays.toString(B));
+    }
+
+    public int[] solution(int[] A)
+    {
+        Set<Integer> setA = asSet(A);
+        List<Set<Integer>> divisors = computeDivisors(A.length * 2);
+        int occurrences[] = computeOccurrences(A);
+        int nonDivisors[] = new int[A.length];
+        for (int i=0; i<A.length; i++)
+        {
+            int value = A[i];
+            Set<Integer> d = divisors.get(value);
+            int totalOccurances = 0;
+            for (Integer divisor : d)
+            {
+                if (setA.contains(divisor))
+                {
+                    totalOccurances += occurrences[divisor];
+                }
             }
-        if (peaksCount == 0)
-            return 0;
-        peaks = Arrays.copyOfRange(peaks, 0, peaksCount);
-        for (int sliceSize = 1; sliceSize < n; sliceSize++) {
-            if (n % sliceSize != 0)
-                continue;
-            int slices = n / sliceSize;
-            boolean containsPeaks = true;
-            int peakFound = 0;
-            for (int peak : peaks)
-                if (peak / sliceSize >  peakFound) {
-                    containsPeaks = false;
-                    break;
-                } else if (peak / sliceSize ==  peakFound)
-                    peakFound++;
-            if (peakFound != slices)
-                containsPeaks = false;
-            if (containsPeaks)
-                return slices;
+            nonDivisors[i] = A.length-totalOccurances;
         }
-        return 1;
+        return nonDivisors;
+    }
+
+    private static Set<Integer> asSet(int A[])
+    {
+        Set<Integer> result = new HashSet<Integer>();
+        for (int value : A)
+        {
+            result.add(value);
+        }
+        return result;
+    }
+
+    private static List<Set<Integer>> computeDivisors(int maxValue)
+    {
+        List<Boolean> prime = new ArrayList<Boolean>();
+        prime.addAll(Collections.nCopies(maxValue+1, Boolean.TRUE));
+        List<Set<Integer>> divisors = new ArrayList<Set<Integer>>();
+        for (int i = 0; i < maxValue + 1; i++)
+        {
+            Set<Integer> d = new HashSet<Integer>();
+            d.add(1);
+            d.add(i);
+            divisors.add(d);
+        }
+        for (int i = 2; i <= maxValue; i++)
+        {
+            int next = i + i;
+            while (next <= maxValue)
+            {
+                divisors.get(next).addAll(divisors.get(i));
+                prime.set(next, Boolean.FALSE);
+                next += i;
+            }
+        }
+        return divisors;
+    }
+
+    private static int[] computeOccurrences(int A[])
+    {
+        int occurances[] = new int[A.length * 2 + 1];
+        for (int i=0; i<A.length; i++)
+        {
+            int value = A[i];
+            occurances[value]++;
+        }
+        return occurances;
     }
 }
