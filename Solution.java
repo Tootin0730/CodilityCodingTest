59c002c22233d4ab31bd1a284a0e8a796d7c9bdb
Tootin0730 <yisan1004@gmail.com>
2024-02-15 10:07:11 -0500

    Euclidean algorithm - ChocolatesByNumbers
    Final version

--- Solution.java
+++ Solution.java
@@ -1,39 +1,23 @@
+import java.util.*;
+
 class Solution {
-    public int[] solution(int N, int[] P, int[] Q) {
-        int[] F = new int[N+1];
-        int f = 2;
-        while (f * f <= N) {
-            if (F[f] == 0) {
-                int K = f * f;
-                while (K <= N) {
-                    if (F[K] == 0) {
-                        F[K] = f;
-                    }
-                    K += f;
-                }
-            }
-            f += 1;
-        }
+    public int solution(int N, int M) {
 
-        int M = P.length;
-        int[] isSemiprime = new int[N + 1];
-        for (int i = 0; i < M; i++) {
-            int start = P[i], end = Q[i];
-            P[i] = 0;
-            for (int j = start; j <= end; j++) {
-                if (isSemiprime[j] == 0){       
-                    isSemiprime[j] = -1;        
-                    if (F[j] > 0) {
-                        int x = j / F[j];
-                        if (F[j / F[j]] == 0) {
-                            isSemiprime[j] = 1;
-                        }
-                    }
-                }
-                P[i] += isSemiprime[j] > 0 ? 1 : 0;
+        int answer =1;
+        HashMap<Integer , Integer> som = new HashMap<>();
+        boolean check = true;
+        int x =0;
+        som.put(0,0);
+        while(check){
+        int  m = (x+ M) % N ;
+            x = m;
+            if(som.containsKey(x)){
+            check = false;
+            }else{
+                som.put(x,0);
+                answer++;
             }
         }
-
-        return P;
+        return answer;
     }
 }
