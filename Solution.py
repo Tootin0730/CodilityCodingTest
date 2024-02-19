92c3b71ca4f555d3e5c698baae451b5da1b49f35
Tootin0730 <yisan1004@gmail.com>
2024-02-19 12:11:00 -0500

    Fibonacci numbers -  FibFrog
    Final version

---
+++ Solution.py
@@ -0,0 +1,30 @@
+def solution(A):
+    A.append(1)
+    N = len(A)
+    fib = [0] * 27
+    fib[1] = 1
+    for i in range(2, 27):
+        fib[i] = fib[i-1] + fib[i-2]
+        if fib[i] > N: 
+            fib = fib[2:i]
+            break
+    
+    reachable = [-1]*N
+    for jump in fib:
+        if A[jump-1] == 1: reachable[jump-1] = 1
+    
+    for i in range(N):
+        if A[i] == 1 and reachable[i] < 0:
+            min = N + 1
+            minidx = -1
+            for jump in fib:
+                pre = i - jump
+                if pre < 0 or reachable[pre] <0:
+                    continue
+                if min > reachable[pre]:
+                    min = reachable[pre]
+                    minidx = pre
+            if minidx != -1:
+                reachable[i] = min +1
+    
+    return reachable[-1]
