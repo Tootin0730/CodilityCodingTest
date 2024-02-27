e7b959e127f655b503a45295a716e66f2a2ac8a9
Tootin0730 <yisan1004@gmail.com>
2024-02-27 12:02:15 -0500

    Caterpillar method - AbsDictinct
    Initial try

--- Solution.java
+++ Solution.java
@@ -1,51 +1,15 @@
-import java.util.Arrays;
-
 class Solution {
-    public int solution(int[] A, int[] B, int[] C) {
-        int N = A.length;
-        int M = C.length;
-        
-        int[][] sortedNail = new int[M][2];
-        for (int i = 0; i < M; i++) {
-            sortedNail[i][0] = C[i];
-            sortedNail[i][1] = i;
-        }
-        Arrays.sort(sortedNail, (int x[], int y[]) -> x[0] - y[0]);
-        int result = 0;
-        for (int i = 0; i < N; i++) {
-            result = getMinIndex(A[i], B[i], sortedNail, result);
-            if (result == -1)
-                return -1;
-        }
-        return result + 1;
-    }
-    
-    public int getMinIndex(int startPlank, int endPlank, int[][] nail, int preIndex) {
-        int min = 0;
-        int max = nail.length - 1;
-        int minIndex = -1;
-        while (min <= max) {
-            int mid = (min + max) / 2;
-            if (nail[mid][0] < startPlank)
-                min = mid + 1;
-            else if (nail[mid][0] > endPlank)
-                max = mid - 1;
-            else {
-                max = mid - 1;
-                minIndex = mid;
+    public int solution(int[] A) {
+
+        int total = 0;
+        for(int i = 0; i < A.length; i++){
+            total += 1;
+            for(int j = i+1; j < A.length; j++){
+                if(A[i] == 0-A[j]){
+                    total -= 1;
+                }
             }
         }
-        if (minIndex == -1)
-            return -1;
-        int minIndexOrigin = nail[minIndex][1];
-        
-        for (int i = minIndex; i < nail.length; i++) {
-            if (nail[i][0] > endPlank)
-                break;
-            minIndexOrigin = Math.min(minIndexOrigin, nail[i][1]);
-            if (minIndexOrigin <= preIndex)
-                return preIndex;
-        } 
-        return minIndexOrigin;
+        return total;
     }
 }
